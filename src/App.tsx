128e89d24022009d992829bc78e2d46ed81297b3
LiponRoy <liponbdbd@gmail.com>
2024-02-09 15:33:40 +0600

    project start

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className=" h-screen w-full flex flex-col justify-center items-center">
+        <span className=' text-4xl font-extrabold '>User list</span>
+        <span className=' text-2xl font-extrabold '>Project start</span>
+      </div>
+    </>
+  )
+}
+
+export default App
